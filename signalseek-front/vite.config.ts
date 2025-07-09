ac822cec03ee5a40e78d27af064eaca055478ee4
s0l0n3t <furkantokgz@gmail.com>
2025-07-09 22:00:07 +0300

    asd

--- signalseek-front/vite.config.ts
+++ signalseek-front/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     cors: false,
     proxy: {
       '/api': {
-        target: 'http://54.226.173.145:8180',
+        target: 'http://signalseek.xyz:8180',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, '')
